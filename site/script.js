f8da73ba6db02fe7ad6aca8ec00a22555238b163
Gustavo Moreira <guunstg420@gmail.com>
2022-12-03 14:17:04 -0300

    lAI LAI GATA

--- site/script.js
+++ site/script.js
@@ -11,19 +11,19 @@ var text2 =
 'Vou apagar lento fodasi 🤬🤬🤬🤬'
 
 var text3 = 
-'QUERO DIZER QUE VOCÊ É UMA GATA E EU TE AMO CABESA DE PICA 👍🏽👍🏽👍🏽👍🏽👍🏽' +
-'OBRIGADO POR TUDO VIU? MUITO BOM ESTAR COM VC MINHA GATINHAAA' +
-'EU SEI QUE VC É ANSIOSA E TEM MEDO DE NÃO CONSEGUIR REALIZAR SEUS SONHOS, E FICA SE COBRANDO MUITO POR ISSO MAS' +
-'SAIBA QUE VC JÁ CONSEGUIU, VC É A MINHA PROGRAMADORA FAVORITA!' +
-'CONTE COMIGO SEMPRE VIU? O MUNDO É TODO SEU!!!'
+'QUERO DIZER QUE VOCÊ É UMA GATA E EU TE AMO CABESA DE PICA 👍🏽👍🏽👍🏽👍🏽👍🏽 ' +
+'OBRIGADO POR TUDO VIU? MUITO BOM ESTAR COM VC MINHA GATINHAAA ' +
+'EU SEI QUE VC É ANSIOSA E TEM MEDO DE NÃO CONSEGUIR REALIZAR SEUS SONHOS, E FICA SE COBRANDO MUITO POR ISSO MAS ' +
+'SAIBA QUE VC JÁ CONSEGUIU, VC É A MINHA PROGRAMADORA FAVORITA! ' +
+'CONTE COMIGO SEMPRE VIU? O MUNDO É TODO SEU!!! '
 
 var text4 = 
-' QUER UMA FRASE FOFA? VOU ESCREVER, (UM MONTE DE EMOJI APAIXONADO) parei de pedir presente pro papai noel quando vc apareceu (UM MONTE DE EMOJI APAIXONADO) 💘💘💘💘💘💘💘💘💘💘💘💘💘💘💘💘💘💘'
+'QUER UMA FRASE FOFA? VOU ESCREVER, (UM MONTE DE EMOJI APAIXONADO) parei de pedir presente pro papai noel quando vc apareceu (UM MONTE DE EMOJI APAIXONADO) 💘💘💘💘💘💘💘💘💘💘💘💘💘💘💘💘💘💘 '
 
-typewriter.typeString('Boa noite pessaoll, vou mostrar a pessoa mais linda desse mundo. CONFIRA:')
+typewriter.typeString('Boa noite pessaoll, vou mostrar a pessoa mais linda desse mundo. CONFIRA: ')
     .pauseFor(2500)
     .deleteAll()   
-    .typeString(' Sim, criei um site pra elogiar a gata que você é hehe, ta dando uma risadinha né cabesa de pica? kkkkk')
+    .typeString('Sim, criei um site pra elogiar a gata que você é hehe, ta dando uma risadinha né cabesa de pica? kkkkk ')
     .pauseFor(1000)
     .deleteAll()
     .typeString('')
